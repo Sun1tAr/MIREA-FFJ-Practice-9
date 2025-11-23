596532118158aa2b81452fc6dae1168c697b36b2
Sun1tAr <denchik2376@yandex.ru>
2025-11-23 16:55:13 +0300

    README + refactoring

--- src/main/java/my/learn/mireaffjpractice9/exception/handler/MyExceptionHandler.java
+++ src/main/java/my/learn/mireaffjpractice9/exception/handler/MyExceptionHandler.java
@@ -1,9 +1,12 @@
 package my.learn.mireaffjpractice9.exception.handler;
 
+import jakarta.validation.ValidationException;
 import my.learn.mireaffjpractice9.exception.AppException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,6 +27,18 @@ public class MyExceptionHandler {
         HashMap<String, Object> body = new HashMap<>();
         body.put("message", exception.getMessage());
         return new ResponseEntity<>(body, exception.getStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleException(BindException exception) {
+        HashMap<String, Object> body = new HashMap<>();
+        HashMap<String, Object> errors = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach((fieldError) -> {errors.put(fieldError.getField(), fieldError.getDefaultMessage());});
+
+        body.put("message", "Validation failed");
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
 }
